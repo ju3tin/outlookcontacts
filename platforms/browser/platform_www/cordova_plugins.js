359e35b846abbf844a6939bf91892c93bc17220b
justin <justin@impact-it.tech>
2020-03-27 07:26:36 +0000

    dude

--- platforms/browser/platform_www/cordova_plugins.js
+++ platforms/browser/platform_www/cordova_plugins.js
@@ -1,9 +1,35 @@
 cordova.define('cordova/plugin_list', function(require, exports, module) {
-module.exports = [];
+module.exports = [
+    {
+        "file": "plugins/cordova-plugin-inappbrowser/www/inappbrowser.js",
+        "id": "cordova-plugin-inappbrowser.inappbrowser",
+        "pluginId": "cordova-plugin-inappbrowser",
+        "clobbers": [
+            "cordova.InAppBrowser.open",
+            "window.open"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-inappbrowser/src/browser/InAppBrowserProxy.js",
+        "id": "cordova-plugin-inappbrowser.InAppBrowserProxy",
+        "pluginId": "cordova-plugin-inappbrowser",
+        "runs": true
+    },
+    {
+        "file": "plugins/phonegap-plugin-oauth2/www/pg-oauth2.js",
+        "id": "phonegap-plugin-oauth2.PG_OAuth2",
+        "pluginId": "phonegap-plugin-oauth2",
+        "clobbers": [
+            "PG_OAuth2"
+        ]
+    }
+];
 module.exports.metadata = 
 // TOP OF METADATA
 {
-    "cordova-plugin-whitelist": "1.3.4"
+    "cordova-plugin-whitelist": "1.3.4",
+    "cordova-plugin-inappbrowser": "1.7.2",
+    "phonegap-plugin-oauth2": "0.0.1"
 }
 // BOTTOM OF METADATA
 });
---
+++ platforms/browser/www/plugins/phonegap-plugin-oauth2/www/pg-oauth2.js
@@ -0,0 +1,31 @@
+cordova.define("phonegap-plugin-oauth2.PG_OAuth2", function(require, exports, module) { /* global cordova:false */
+/* globals window */
+
+var exec = cordova.require('cordova/exec'),
+    utils = cordova.require('cordova/utils');
+
+var PG_OAuth2 = {
+    authenticate: function(successCallback, errorCallback, startURL, endURL) {
+        // TODO: verify args
+        var browser = cordova.InAppBrowser.open(startURL, '_blank', 'location=yes');
+        browser.addEventListener('loadstart', function(evt){
+            console.log('evt.url = ' + evt.url);
+            if(evt.url.indexOf(endURL) == 0) {
+                successCallback(evt.url);
+                browser.close();
+            }
+        });
+
+        //browser.addEventListener('loadstop', loadStopCallBack);
+
+        browser.addEventListener('loaderror', function(err) {
+            console.log("error " + err)
+            errorCallback(err);
+        });
+
+    }
+};
+
+module.exports = PG_OAuth2;
+
+});
